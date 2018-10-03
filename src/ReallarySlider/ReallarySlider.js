16f718204001af3e9008c3168b210da67d9c83a5
Aaron Endsley <aaronendsley@gmail.com>
2018-10-03 08:52:54 -0500

    started the slider component

---
+++ src/ReallarySlider/ReallarySlider.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ReallarySlider = props => <div className="reallary-slider-container" />;
+
+export default ReallarySlider;
---
+++ src/data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    src: './testpictures/test1.png',
+    altText: 'super cool picture'
+  },
+  {
+    src: './testpictures/test2.png',
+    altText: 'super cool picture'
+  },
+  {
+    src: './testpictures/test3.png',
+    altText: 'super cool picture'
+  },
+  {
+    src: './testpictures/test4.png',
+    altText: 'super cool picture'
+  }
+];
+
+export default data;
